71ab173974964b855419a2d220ef341407ca1a4c
Kevin Fernandes <kfernandes@gmail.com>
2021-05-30 04:08:40 -0400

    arrays

---
+++ 8kyu/SwapValues.js
@@ -0,0 +1,19 @@
+/***
+ Description:
+I would like to be able to pass an array with two elements to my swapValues function to swap the values. However it appears that the values aren't changing.
+
+Can you figure out what's wrong here?
+
+BUGS    ARRAYS  FUNDAMENTALS
+*/
+function swapValues(args) {
+    var temp = args[0];
+    args[0] = args[1];
+    args[1] = temp;
+  }
+
+/* // alternative solution:
+function swapValues() {
+    return arguments[0].reverse();
+  }
+*/
